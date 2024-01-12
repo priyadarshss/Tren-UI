f24489499584e6f39e526505496f482e73ef82a1
Priyadarsh S S <priyadarsh2001@gmail.com>
2024-01-12 22:52:59 +0530

    feat: Add yarn configuration and update Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,9 @@
-export const index = () => {
+import { ComponentProps } from 'react'
+
+type ButtonProps = ComponentProps<'button'>
+
+export const Button = ({ ...props }: ButtonProps) => {
   return (
-    <button className="text-red-500">index</button>
+    <button className='text-red-500' {...props} />
   )
 }
